52b5011eeaf37d86059095a259db2beb06cbb5cb
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-02 16:59:30 +0200

    megaphone v4 >:)

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 16:37:52 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/02 16:54:35 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/02 16:58:15 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ int main(int ac, char *av[])
 		return (0);
 	}
 	for (int j = 1; j < ac; j++){
-		voice = av[j];
+		voice = j<:av:>;
 		for (size_t i = 0; i < voice.size(); i++){
 			voice[i] = std::toupper(voice[i]);
 		}
