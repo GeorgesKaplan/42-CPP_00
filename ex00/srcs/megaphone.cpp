ce08eb17bc31b0ec0ec273cf784416c13bd7452a
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-02 16:51:46 +0200

    megaphone v2

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 16:37:52 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/02 16:43:41 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/02 16:51:14 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,11 +16,19 @@
 
 int main(int ac, char *av[])
 {
+	(void)av;
 	std::string noise = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 	std::string voice = "";
 
-	if (ac < 1){
+	if (ac < 2){
 		std::cout << noise << std::endl;
 		return (0);
+	}
+	for (size_t j = 1; j < ac - 1; j++){
+		voice = av[j];
+		for (size_t i = 0; i < voice.size(); i++){
+			voice[i] = std::toupper(voice[i]);
+		}
+		std::cout << voice << std::endl;
 	}
 }
