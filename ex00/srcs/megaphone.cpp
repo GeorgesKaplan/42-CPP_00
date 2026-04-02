92916ab2ed3fcbc017c37ad58e503ee968e48cb2
GeorgesKaplan <dimitri.nantet@gmail.com>
2026-04-02 16:44:15 +0200

    megaphone is back

--- ex00/srcs/megaphone.cpp
+++ ex00/srcs/megaphone.cpp
@@ -6,7 +6,21 @@
 /*   By: dnantet <dnantet@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/02 16:37:52 by dnantet           #+#    #+#             */
-/*   Updated: 2026/04/02 16:37:55 by dnantet          ###   ########.fr       */
+/*   Updated: 2026/04/02 16:43:41 by dnantet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include <string>
+#include <cctype>
+
+int main(int ac, char *av[])
+{
+	std::string noise = "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::string voice = "";
+
+	if (ac < 1){
+		std::cout << noise << std::endl;
+		return (0);
+	}
+}
